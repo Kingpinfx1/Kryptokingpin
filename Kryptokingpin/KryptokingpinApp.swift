e3808eb03a3b68e4964089c0b884ef7db8f18307
kingpin <kingpinfx1@gmail.com>
2025-04-21 18:26:00 +0100

    Initial Commit

---
+++ Kryptokingpin/KryptokingpinApp.swift
@@ -0,0 +1,17 @@
+//
+//  KryptokingpinApp.swift
+//  Kryptokingpin
+//
+//  Created by kingpin on 4/21/25.
+//
+
+import SwiftUI
+
+@main
+struct KryptokingpinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
